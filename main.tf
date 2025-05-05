57c7214d84b62e1d2975b0e9d012e1d710fd00b4
Brian Tyndall <brian.tyndall@outlook.com>
2025-05-05 01:19:33 +0100

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  backend "local" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.117.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "bootstrap_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "bootstrap_storage" {
+  name                            = var.storage_account_name
+  resource_group_name             = azurerm_resource_group.bootstrap_rg.name
+  location                        = azurerm_resource_group.bootstrap_rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_storage_container" "bootstrap_container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.bootstrap_storage.name
+  container_access_type = "private"
+}
